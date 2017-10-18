9da559313786e30651a7ab3562dfa03d81c98807
Matias Camiletti <matias.camiletti@gmail.com>
2017-10-18 17:52:49 -0300

    Commit inicial.

---
+++ push/src/main/java/com/mobileia/push/MobileiaPush.java
@@ -0,0 +1,12 @@
+package com.mobileia.push;
+
+/**
+ * Created by matiascamiletti on 18/10/17.
+ */
+
+public class MobileiaPush {
+    /**
+     * Alamcena la URL del servicio
+     */
+    public static final String BASE_URL = "http://push.mobileia.com:8080/";
+}
